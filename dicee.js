14e84a8739e793c446d48ccbf0cc428c63ef0bbe
Nigarsenpai <85122388+Nigarsenpai@users.noreply.github.com>
2021-07-18 16:12:49 +0400

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -20,6 +20,6 @@ else if(RandomNumber1<RandomNumber2){
    document.querySelector("h1").innerHTML="Player 2 Wins!"
  }
 else{
-   document.querySelector("h1").innerHTML="Dice!"
+   document.querySelector("h1").innerHTML="Draw!"
 }
 }
